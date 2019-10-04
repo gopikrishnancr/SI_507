77812d57fc856ac407453114428b1e62d6a8322d
gopikrishnancr <54066645+gopikrishnancr@users.noreply.github.com>
2019-10-04 15:24:01 +0530

    Add files via upload

---
+++ solve_gseidel.m
@@ -0,0 +1,29 @@
+function x = solve_gseidel(A,b,x0,tol,kmax)
+ 
+  N = size(A,1);               % get the dimension of the system
+  x = x0;                      
+  Atemp = zeros(N,N);
+  Btemp = zeros(N,1);
+  b = b./diag(A);
+  A = -bsxfun(@rdivide,A,diag(A)); % matrix-vector pairwise multiplication
+  A = A - diag(diag(A));           % eliminates diagonal entries of A
+  
+  for k =1:kmax
+   
+    for i = 1:N
+      x(i) = A(i,:)*x + b(i);         % Gauss-Seidel loop
+    end
+ 
+    if norm(x - x0)/norm(x0 + eps) < tol % relative error (why eps?)
+      break
+    endif
+      x0 = x;
+  endfor
+  
+  fprintf('\n Relative eroor is %e \n',norm(x - x0)/norm(x0 + eps));
+  
+  if k == kmax % error message on maximum iteration number reaching
+    disp('Maximum iteration reached. Solution may be erraneous. ');
+  endif
+  
+endfunction
---
+++ solve_thomas.m
@@ -0,0 +1,16 @@
+function x = solve_thomas(A,d)
+  na = size(A,1); % number of rows in A
+  
+  for i = 2:na
+    p = A(i,i-1)/A(i-1,i-1);           % p = a_i/b_{i-1}
+    A(i,i) = A(i,i) - p*A(i-1,i);     % b_i = b_i - p*c_{i-1} 
+    d(i,1) = d(i,1) - p*d(i-1,1);      % d_i = d_i - p*d_{i-1} 
+  endfor
+  
+  x = zeros(na,1);
+  x(na,1) = d(na,1)/A(na,na);
+  for i = na-1:-1:1
+    x(i,1) = (d(i,1) - A(i,i+1)*x(i+1,1))/A(i,i);
+    endfor
+  
+endfunction
